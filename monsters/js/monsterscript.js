7d5e1da699c922b0080bc1326d34bcdbf377f9ef
Reaver01 <reaver_01@gmx.com>
2016-04-24 14:02:10 +0200

    New Monsters/Armor

--- monsters/js/monsterscript.js
+++ monsters/js/monsterscript.js
@@ -1,4 +1,7 @@
-function materialdrops(arr) {
+var materialIcons = ["not_interested", "thumb_down", "thumbs_up_down", "thumb_up", "grade",];
+
+$(function() {
+	var arr = materialArray;
 	var out = '';
 	var i;
 	for(i = 0; i < arr.length; i ++) {
@@ -13,44 +16,35 @@ function materialdrops(arr) {
 	'</tr>';
 	}
 	document.getElementById("material-drops").innerHTML = out;
-};
-function monsterContent1(arr) {
-	var out = "";
+	var arr = weaknessArray;
+	var out = '';
 	var i;
 	for(i = 0; i < arr.length; i ++) {
 		out += '' +
 	'<tr>' +
-		'<td>' + arr[i].data[0] + '</td>' +
-		'<td><i class="' + arr[i].data[1] + ' material-icons flow-text">' + arr[i].data[1] + '</i></td>' +
-		'<td><i class="' + arr[i].data[2] + ' material-icons flow-text">' + arr[i].data[2] + '</i></td>' +
-		'<td><i class="' + arr[i].data[3] + ' material-icons flow-text">' + arr[i].data[3] + '</i></td>' +
-		'<td><i class="' + arr[i].data[4] + ' material-icons flow-text">' + arr[i].data[4] + '</i></td>' +
-		'<td><i class="' + arr[i].data[5] + ' material-icons flow-text">' + arr[i].data[5] + '</i></td>' +
-		'<td><i class="' + arr[i].data[6] + ' material-icons flow-text">' + arr[i].data[6] + '</i></td>' +
-		'<td><i class="' + arr[i].data[7] + ' material-icons flow-text">' + arr[i].data[7] + '</i></td>' +
-		'<td><i class="' + arr[i].data[8] + ' material-icons flow-text">' + arr[i].data[8] + '</i></td>' +
+		'<td>' + arr[i].data[2] + '</td>' +
+		'<td><i class="weak' + arr[i].data[3] + ' material-icons flow-text">' + materialIcons[arr[i].data[3]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[4] + ' material-icons flow-text">' + materialIcons[arr[i].data[4]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[5] + ' material-icons flow-text">' + materialIcons[arr[i].data[5]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[6] + ' material-icons flow-text">' + materialIcons[arr[i].data[6]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[7] + ' material-icons flow-text">' + materialIcons[arr[i].data[7]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[8] + ' material-icons flow-text">' + materialIcons[arr[i].data[8]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[9] + ' material-icons flow-text">' + materialIcons[arr[i].data[9]] + '</i></td>' +
+		'<td><i class="weak' + arr[i].data[10] + ' material-icons flow-text">' + materialIcons[arr[i].data[10]] + '</i></td>' +
 	'</tr>';
 	}
 	document.getElementById("monster1").innerHTML = out;
-};
-function monsterContent2(arr) {
-	var out = "";
-	var i;
-	for(i = 0; i < arr.length; i ++) {
-		out += '' +
-	'<tr>' +
-		'<td><i class="' + arr[i].data[0] + ' material-icons flow-text">' + arr[i].data[0] + '</i></td>' +
-		'<td><i class="' + arr[i].data[1] + ' material-icons flow-text">' + arr[i].data[1] + '</i></td>' +
-		'<td><i class="' + arr[i].data[2] + ' material-icons flow-text">' + arr[i].data[2] + '</i></td>' +
-		'<td><i class="' + arr[i].data[3] + ' material-icons flow-text">' + arr[i].data[3] + '</i></td>' +
-		'<td><i class="' + arr[i].data[4] + ' material-icons flow-text">' + arr[i].data[4] + '</i></td>' +
-		'<td><i class="' + arr[i].data[5] + ' material-icons flow-text">' + arr[i].data[5] + '</i></td>' +
-		'<td><i class="' + arr[i].data[6] + ' material-icons flow-text">' + arr[i].data[6] + '</i></td>' +
-		'<td><i class="' + arr[i].data[7] + ' material-icons flow-text">' + arr[i].data[7] + '</i></td>' +
-		'<td class="hide-on-small-only">' + arr[i].data[8] + '</td>' +
+	var out = '<tr>' +
+		'<td><i class="weak' + arr[0].data[11] + ' material-icons flow-text">' + materialIcons[arr[0].data[11]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[12] + ' material-icons flow-text">' + materialIcons[arr[0].data[12]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[13] + ' material-icons flow-text">' + materialIcons[arr[0].data[13]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[14] + ' material-icons flow-text">' + materialIcons[arr[0].data[14]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[15] + ' material-icons flow-text">' + materialIcons[arr[0].data[15]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[16] + ' material-icons flow-text">' + materialIcons[arr[0].data[16]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[17] + ' material-icons flow-text">' + materialIcons[arr[0].data[17]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[18] + ' material-icons flow-text">' + materialIcons[arr[0].data[18]] + '</i></td>' +
+		'<td><i class="weak' + arr[0].data[20] + ' material-icons flow-text">' + materialIcons[arr[0].data[20]] + '</i></td>' +
 	'</tr>';
-		out2 = arr[i].data[8]
-	}
 	document.getElementById("monster2").innerHTML = out;
-	document.getElementById("monster3").innerHTML = out2;
-};
+	document.getElementById("monster3").innerHTML = arr[0].data[19];
+});
---
+++ monsters/js/navdata.js
@@ -0,0 +1,482 @@
+var lowrankArray = [
+			{
+				"data": [
+					"Bulldrome",
+					"bulldrome",
+					"bulldrome.png",
+					"野猪王",
+				]
+			},
+			{
+				"data": [
+					"Velocidrome",
+					"velocidrome",
+					"velocidrome.png",
+					"蓝速龙王",
+				]
+			},
+			{
+				"data": [
+					"Caeserber",
+					"caeserber",
+					"caeserber.png",
+					"河狸兽",
+				]
+			},
+			{
+				"data": [
+					"Yian Kut-Ku",
+					"yian-kut-ku",
+					"yiankutku.png",
+					"大怪鸟",
+				]
+			},
+			{
+				"data": [
+					"Gendrome",
+					"gendrome",
+					"gendrome.png",
+					"黄速龙王",
+				]
+			},
+			{
+				"data": [
+					"Baelidae",
+					"baelidae",
+					"baelidae.png",
+					"鬼狩蛛",
+				]
+			},
+			{
+				"data": [
+					"Yellow Caeserber",
+					"yellow-caeserber",
+					"caeserber2.png",
+					"沙狸兽",
+				]
+			},
+			{
+				"data": [
+					"Gypceros",
+					"gypceros",
+					"gypceros.png",
+					"毒怪鸟",
+				]
+			},
+			{
+				"data": [
+					"Congalala",
+					"congalala",
+					"congalala.png",
+					"桃毛兽王",
+				]
+			},
+			{
+				"data": [
+					"Chramine",
+					"chramine",
+					"chramine.png",
+					"沙雷鸟",
+				]
+			},
+			{
+				"data": [
+					"Cephadrome",
+					"cephadrome",
+					"cephadrome.png",
+					"沙龙王",
+				]
+			},
+			{
+				"data": [
+					"Daimyo Hermitaur",
+					"daimyo-hermitaur",
+					"daimyohermitaur.png",
+					"大名盾蟹",
+				]
+			},
+			{
+				"data": [
+					"Blue Yian Kut-Ku",
+					"blue-yian-kut-ku",
+					"yiankutku2.png",
+					"青怪鸟",
+				]
+			},
+			{
+				"data": [
+					"Khezu",
+					"khezu",
+					"khezu.png",
+					"电龙",
+				]
+			},
+			{
+				"data": [
+					"Basarios",
+					"basarios",
+					"basarios.png",
+					"岩龙",
+				]
+			},
+			{
+				"data": [
+					"Hypnocatrice",
+					"hypnocatrice",
+					"hypnocatrice.png",
+					"眠鸟",
+				]
+			},
+			{
+				"data": [
+					"Gold Congalala",
+					"gold-congalala",
+					"congalala2.png",
+					"金毛兽王",
+				]
+			},
+			{
+				"data": [
+					"Dread Baelidae",
+					"dread-baelidae",
+					"baelidae2.png",
+					"骇狩蛛",
+				]
+			},
+			{
+				"data": [
+					"Purple Gypceros",
+					"purple-gypceros",
+					"gypceros2.png",
+					"紫毒鸟",
+				]
+			},
+			{
+				"data": [
+					"Shogun Ceanataur",
+					"shogun-ceanataur",
+					"shogunceanataur.png",
+					"将军镰蟹",
+				]
+			},
+			{
+				"data": [
+					"Blangonga",
+					"blangonga",
+					"blangonga.png",
+					"雪狮子王",
+				]
+			},
+			{
+				"data": [
+					"Estrellian",
+					"estrellian",
+					"estrellian.png",
+					"星龙",
+				]
+			}];
+var highrankArray = [
+			{
+				"data": [
+					"Ice Chramine",
+					"ice-chramine",
+					"chramine2.png",
+					"冰雷鸟",
+				]
+			},
+			{
+				"data": [
+					"Silver Hypnocatrice",
+					"silver-hypnocatrice",
+					"hypnocatrice2.png",
+					"银眠鸟",
+				]
+			},
+			{
+				"data": [
+					"Akura Vashimu",
+					"akura-vashimu",
+					"akuravashimu.png",
+					"尾晶蝎",
+				]
+			},
+			{
+				"data": [
+					"Yian Garuga",
+					"yian-garuga",
+					"yiangaruga.png",
+					"黑狼鸟",
+				]
+			},
+			{
+				"data": [
+					"Red Khezu",
+					"red-khezu",
+					"khezu2.png",
+					"红电龙",
+				]
+			},
+			{
+				"data": [
+					"Monoblos",
+					"monoblos",
+					"monoblos.png",
+					"一角龙",
+				]
+			},
+			{
+				"data": [
+					"Doom Estrellian",
+					"doom-estrellian",
+					"estrellian2.png",
+					"祸星龙",
+				]
+			},
+			{
+				"data": [
+					"Rathian",
+					"rathian",
+					"rathian.png",
+					"雌火龙",
+				]
+			},
+			{
+				"data": [
+					"Iodrome",
+					"iodrome",
+					"iodrome.png",
+					"红速龙王",
+				]
+			},
+			{
+				"data": [
+					"Giadrome",
+					"giadrome",
+					"giadrome.png",
+					"白速龙王",
+				]
+			},
+			{
+				"data": [
+					"Swordmaster Shogun Ceanataur",
+					"swordmaster-shogun-ceanataur",
+					"shogunceanataur.png",
+					"剑豪镰蟹",
+				]
+			},
+			{
+				"data": [
+					"Ghost Caeserber",
+					"ghost-caeserber",
+					"caeserber.png",
+					"战鬼河狸兽",
+				]
+			},
+			{
+				"data": [
+					"Crystal Basarios",
+					"crystal-basarios",
+					"basarios.png",
+					"晶岩龙",
+				]
+			},
+			{
+				"data": [
+					"One-Eared Yian Garuga",
+					"one-eared-yian-garuga",
+					"yiangaruga.png",
+					"独耳黑狼鸟",
+				]
+			},
+			{
+				"data": [
+					"Conflagration Rathian",
+					"conflagration-rathian",
+					"rathian.png",
+					"烈焰女王",
+				]
+			},
+			{
+				"data": [
+					"Shattered Monoblos",
+					"shattered-monoblos",
+					"monoblos.png",
+					"断刃一角龙",
+				]
+			},
+			{
+				"data": [
+					"Tartaronis",
+					"tartaronis",
+					"tartaronis.png",
+					"吞渊龙",
+				]
+			},
+			{
+				"data": [
+					"Rathalos",
+					"rathalos",
+					"rathalos.png",
+					"雄火龙",
+				]
+			},
+			{
+				"data": [
+					"Shen Gaoren",
+					"shen-gaoren",
+					"shengaoren.png",
+					"砦蟹",
+				]
+			},
+			{
+				"data": [
+					"Tigrex",
+					"tigrex",
+					"tigrex.png",
+					"轰龙",
+				]
+			},
+			{
+				"data": [
+					"Guren Shen Gaoren",
+					"guren-shen-gaoren",
+					"shengaoren2.png",
+					"红莲砦蟹",
+				]
+			},
+			{
+				"data": [
+					"Tepekki Shen Gaoren",
+					"tepekki-shen-gaoren",
+					"shengaoren3.png",
+					"铠岩砦蟹",
+				]
+			},
+			{
+				"data": [
+					"Slicemargl",
+					"slicemargl",
+					"slicemargl.png",
+					"剑极狼",
+				]
+			},
+			{
+				"data": [
+					"Plesioth",
+					"plesioth",
+					"plesioth.png",
+					"水龙",
+				]
+			},
+			{
+				"data": [
+					"Diablos",
+					"diablos",
+					"diablos.png",
+					"角龙",
+				]
+			},
+			{
+				"data": [
+					"Azure Rathalos",
+					"azure-rathalos",
+					"rathalos2.png",
+					"苍火龙",
+				]
+			},
+			{
+				"data": [
+					"Lightenna",
+					"lightenna",
+					"lightenna.png",
+					"电甲虫",
+				]
+			},
+			{
+				"data": [
+					"Kushala Daora",
+					"kushala-daora",
+					"kushaladora.png",
+					"钢龙",
+				]
+			},
+			{
+				"data": [
+					"Akura Jebia",
+					"akura-jebia",
+					"akurajebia.png",
+					"灰晶蝎",
+				]
+			},
+			{
+				"data": [
+					"Gold Hypnocatrice",
+					"gold-hypnocatrice",
+					"hypnocatrice3.png",
+					"金眠鸟",
+				]
+			},
+			{
+				"data": [
+					"Sandstone Basarios",
+					"sandstone-basarios",
+					"basarios2.png",
+					"砂岩龙",
+				]
+			},
+			{
+				"data": [
+					"Pink Rathian",
+					"pink-rathian",
+					"rathian2.png",
+					"樱火龙",
+				]
+			},
+			{
+				"data": [
+					"Merphistophelin",
+					"merphistophelin",
+					"merphistophelin.png",
+					"荒厄龙",
+				]
+			},
+			];
+
+$(function() {
+	var low = lowrankArray;
+	var high = highrankArray;
+	var outlow = '<span class="card-title">Low/High Rank Monsters</span><br>';
+	var outhigh = '<span class="card-title">High Rank Monsters</span><br>';
+	var i;
+
+	for(i = 0; i < low.length; i ++) {
+		outlow += ''+
+	'<a class="tooltipped" data-position="right" data-delay="10" data-tooltip="'+
+	low[i].data[0]+
+	'" href="../'+
+	low[i].data[1]+
+	'/"><div class="chip"><img src="../images/'+
+	low[i].data[2]+
+	'" alt="'+
+	low[i].data[0]+
+	'">'+
+	low[i].data[3]+
+	'</div></a>'
+	}
+	for(i = 0; i < high.length; i ++) {
+		outhigh += ''+
+	'<a class="tooltipped" data-position="right" data-delay="10" data-tooltip="'+
+	high[i].data[0]+
+	'" href="../'+
+	high[i].data[1]+
+	'/"><div class="chip"><img src="../images/'+
+	high[i].data[2]+
+	'" alt="'+
+	high[i].data[0]+
+	'">'+
+	high[i].data[3]+
+	'</div></a>'
+	}
+	document.getElementById("lowranknav").innerHTML = outlow;
+	document.getElementById("highranknav").innerHTML = outhigh;
+});
